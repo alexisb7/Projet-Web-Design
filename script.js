74353eaf087b302695ffd18a6ef5b570be7101df
Alexis Bériot <alexis.beriot@epfedu.fr>
2022-04-28 16:16:43 +0200

    add comments

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-class Game {
+// Script pour le jeu de Ball-trap 
 
+//Classe Game pour trier les scores
+class Game {
+//on utilise une classe ici pour lier les noms/scores/temps/nbcibles entre eux via un objet regroupant toutes ces données
+//on peut ensuite trier les scores correspondant à une partie donnée avec le nom du joueur, son temps, etc..
+
+    //Constructeur de la classe
     constructor(name, score, targets, time) {
         this.name = name;
         this.score = score;
@@ -7,16 +13,17 @@ class Game {
         this.time = time;
     }
 
+    //Méthode de tri décroissante des scores 
     sortGames(tabGames) {
         tabGames.sort((x, y) => y.score - x.score);
     }
 }
 
-// Script pour le jeu de Ball-trap 
 var time = 0; //timer variable
 var nb_target = 0; //nombre de cibles 
 var chrono = null; //id du chrono
 
+//Affiche le tableau des scores au chargement de la page
 if (document.readyState === 'complete') {
     printScores();
 } 
@@ -75,7 +82,7 @@ function startChrono() {
     chrono = setInterval(updateChrono, 100);//incrémente le chrono toutes les 100ms
 }
 
-function updateChrono() {
+function updateChrono() { //Maj de l'affichage du chrono
     time++;//temps en dixième de seconds
     //éléments HTML du chrono
     var tenth = document.getElementById("tenth");
@@ -122,7 +129,7 @@ function updateChrono() {
     }
 }
 
-function clearGround() {
+function clearGround() { //Nettoie le terrrain 
     clearInterval(chrono);
     time = 0;
     document.getElementById("minutes").innerHTML = "0";
@@ -132,12 +139,12 @@ function clearGround() {
     var targets = document.getElementsByClassName("target");
     for(var j = 0; j < targets.length; j++) {
         targets[j].style.opacity = 0;
-        terrain.removeChild(targets[j]);
+        terrain.removeChild(targets[j]); //supprime les éléments enfants du terrain
         j--;
     }
 }
 
-function endGame() {
+function endGame() { //Fin du jeu / Affichage du score
     if(nb_target == 0){
         clearInterval(chrono);
         saveGame();
@@ -145,15 +152,16 @@ function endGame() {
     }
 }
 
-function saveGame() {
+function saveGame() { //Sauvegarde de la partie jouée
+    //récupère les données de la partie
     var name = document.getElementById("nom").value;
     var targets = document.getElementById("nbtargets").value;
-    var score = targets / (time * 0.1);
-    if(localStorage.getItem("list_names") != null){
-        var list_names = localStorage.getItem("list_names");
-        var names = list_names.split(",");
-        names.push(name);
-        localStorage.setItem("list_names", names);
+    var score = targets / (time * 0.1); //score en s 
+    if(localStorage.getItem("list_names") != null){ //vérifie si des parties ont été jouées avant
+        var list_names = localStorage.getItem("list_names"); //récupère le string stockant les noms 
+        var names = list_names.split(","); //création d'un tableau de noms
+        names.push(name); //ajout de la dernière partie
+        localStorage.setItem("list_names", names); //maj de l'item dans le localstorage
         var list_scores = localStorage.getItem("list_scores");
         var scores = list_scores.split(",");
         scores.push(Math.round(score * 1000) / 1000);
@@ -166,51 +174,52 @@ function saveGame() {
         var times = list_times.split(",");
         times.push(Math.round((time * 0.1) * 10) / 10);
         localStorage.setItem("list_times", times);
-        document.getElementById("scoreboard").removeChild(document.getElementById("scoretable"));
-        printScores();
+        document.getElementById("scoreboard").removeChild(document.getElementById("scoretable")); //supprime le précédent tableau des scores
+        printScores(); //maj du tabscores
     }
-    else {
+    else { //aucune partie jouée avant --> 1er score
         localStorage.setItem("list_names", name);
-        localStorage.setItem("list_scores", Math.round(score * 1000) / 1000);
+        localStorage.setItem("list_scores", Math.round(score * 1000) / 1000); //arrondi 3 chiffres après la virgule
         localStorage.setItem("list_targets", targets);
-        localStorage.setItem("list_times", Math.round((time * 0.1) * 10) / 10);
-        document.getElementById("scoreboard").removeChild(document.getElementById("scoretable"));
-        printScores();
+        localStorage.setItem("list_times", Math.round((time * 0.1) * 10) / 10); //arrondi à 1 chiffre près
+        document.getElementById("scoreboard").removeChild(document.getElementById("scoretable")); 
+        printScores(); 
     }
 }
 
-function printScores() {
+function printScores() { //Affichage des scores
+    //récupère les données de la partie
     var name = document.getElementById("nom").value;
     var targets = document.getElementById("nbtargets").value;
     var score = Math.round((targets / (time * 0.1)) * 1000) / 1000;
-    var scoreboard = document.getElementById("scoreboard");
-    var table = document.createElement("table");
+    var table = document.createElement("table"); //création d'un élément tableau
     table.id = "scoretable";
+    //affectation de styles sur le tableau
     table.style.marginTop = "10px";
-    table.style.width = "449px";
+    table.style.width = "464px";
     table.style.borderCollapse = "collapse";
     table.style.border = "solid rgba(92, 114, 69, 0.952)";
-    var thead = table.createTHead();
-    var row = thead.insertRow();
+    var thead = table.createTHead(); //création d'en-tetes sur le tableau
+    var row = thead.insertRow(); //ajout d'une ligne
     row.style.backgroundColor = "rgba(161, 192, 127, 0.952)";
     row.style.borderBottom = "solid rgba(92, 114, 69, 0.952)";
-    var cell = row.insertCell();
-    cell.appendChild(document.createTextNode("#"));
+    var cell = row.insertCell(); //ajout d'une cellule
+    cell.appendChild(document.createTextNode("#")); //première colonne : classement
     cell.style.borderRight = "solid rgba(92, 114, 69, 0.952)";
     cell = row.insertCell();
-    cell.appendChild(document.createTextNode("Nom"));
+    cell.appendChild(document.createTextNode("Nom")); //deuxieme colonne : nom
     cell.style.borderRight = "solid rgba(92, 114, 69, 0.952)";
     cell = row.insertCell();
-    cell.appendChild(document.createTextNode("Score"));
+    cell.appendChild(document.createTextNode("Score (cibles/s)")); //troisieme colonne : score
     cell.style.borderRight = "solid rgba(92, 114, 69, 0.952)";
     cell = row.insertCell();
-    cell.appendChild(document.createTextNode("Cibles"));
-    cell.style.borderRight = "solid rgba(92, 114, 69, 0.952)";
+    cell.appendChild(document.createTextNode("Cibles")); //quatrieme colonne : nb de cibles
+    cell.style.borderRight = "solid rgba(92, 114, 69, 0.952)"; 
     cell = row.insertCell();
-    cell.appendChild(document.createTextNode("Temps"));
+    cell.appendChild(document.createTextNode("Temps")); //cinquieme colonne : temps
     cell.style.borderRight = "solid rgba(92, 114, 69, 0.952)";
-    if(localStorage.getItem("list_targets") != null){
-        var tbody = table.createTBody();
+    if(localStorage.getItem("list_targets") != null){ //vérifie si des parties ont déjà été jouées
+        var tbody = table.createTBody(); //création du corps du tableau
         tbody.style.backgroundColor = "#edf0f1";
         var list_names = localStorage.getItem("list_names");
         var names = list_names.split(",");
@@ -220,14 +229,15 @@ function printScores() {
         var t_targets = list_targets.split(",");
         var list_times = localStorage.getItem("list_times");
         var times = list_times.split(",");
-        var tabGames = [];
+        var tabGames = []; //création d'un tableau d'objets Game
         for(var i = 0 ; i < names.length ; i++){
-            var game = new Game(names[i], scores[i], t_targets[i], times[i]);
-            tabGames.push(game);
-            game.sortGames(tabGames)
+            var game = new Game(names[i], scores[i], t_targets[i], times[i]); //création d'un objet Game pour chaque partie jouée
+            tabGames.push(game); //ajout au tableau
+            game.sortGames(tabGames); //appel à la méthode de tri des scores
         }
-        var bool = false;
-        if(tabGames.length > 20){
+        var bool = false; //bool pour vérifier si on a affiché la partie jouée
+        if(tabGames.length > 20){ //controle du nb de parties affichées 
+            //on n'affiche que les 19 meilleures parties et/y compris celle venant d'etre jouée
             for(i = 0; i < 19 ; i++){
                 var index = i+1;
                 row = tbody.insertRow();
@@ -250,12 +260,12 @@ function printScores() {
                 cell = row.insertCell();
                 cell.appendChild(document.createTextNode(tabGames[i].time));
                 cell.style.borderBottom = "solid rgba(92, 114, 69, 0.952)";
-                if(tabGames[i].name == name && tabGames[i].score == score && !bool){
+                if(tabGames[i].name == name && tabGames[i].score == score && !bool){ //colorie la ligne de la partie jouée en vert
                     row.style.backgroundColor = "#80e972";
                     bool = true;
                 }
             }
-            if(bool == false) {
+            if(bool == false) { //si la partie jouée n'est pas dans les 19 meilleures, on l'ajoute en bas du tableau 
                 for(i = 19 ; i < tabGames.length ; i++){
                     if(tabGames[i].name == name && tabGames[i].score == score){
                         row = tbody.insertRow();
@@ -284,7 +294,7 @@ function printScores() {
                 }
             }
         }
-        else {
+        else { //si on a moins de 20 parties jouées, on les affiche toutes
             for(var i = 0; i < tabGames.length ; i++){
                 var index = i+1;
                 row = tbody.insertRow();
@@ -313,10 +323,10 @@ function printScores() {
                 }
             }
         }  
-        scoreboard.appendChild(table);
+        document.getElementById("scoreboard").appendChild(table); //ajout du tableau en tant qu'enfant de l'élément scoreboard
     }
-    else {
-        scoreboard.appendChild(table);
+    else { //si aucune partie jouée, on n'a que les en-tetes du tableau
+        document.getElementById("scoreboard").appendChild(table);
     }
 }
 
